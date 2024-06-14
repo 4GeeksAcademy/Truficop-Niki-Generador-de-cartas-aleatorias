ae041202787593bb682cecae3058cde2f5493892
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-06-14 11:01:22 +0000

    Generador de cartas con boton hecho, faltan los inputs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function symbolGenerator(symbolsList) {
+  const symbols = Math.floor(Math.random() * symbolsList.length);
+  return symbolsList[symbols];
+}
+function numberGenerator(numbersList) {
+  const numbers = Math.floor(Math.random() * numbersList.length);
+  return numbersList[numbers];
+}
+function generateCard() {
+  const SYMBOLS = ["♦", "♥", "♠", "♣"];
+  const NUMBERS = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const randomSymbol = symbolGenerator(SYMBOLS);
+  const randomNumbers = numberGenerator(NUMBERS);
+  const cardHeaderElement = document.getElementById("header-symbol");
+  const cardFooterElement = document.getElementById("footer-symbol");
+  if (randomSymbol === "♦" || randomSymbol === "♥") {
+    cardHeaderElement.classList.add("red");
+    cardFooterElement.classList.add("red");
+  }
+  document.getElementById("header-symbol").innerHTML = randomSymbol;
+  document.getElementById("body-symbol").innerHTML = randomNumbers;
+  document.getElementById("footer-symbol").innerHTML = randomSymbol;
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  setInterval(generateCard, 5000);
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", generateCard);
 };
