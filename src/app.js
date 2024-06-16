e0089511de16bae3b5987aa2b1689c531026952c
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-06-16 19:17:57 +0000

    Ahora permite al usuario especificar el width y height

--- src/app.js
+++ src/app.js
@@ -44,6 +44,81 @@ function generateCard() {
   document.getElementById("body-symbol").innerHTML = randomNumbers;
   document.getElementById("footer-symbol").innerHTML = randomSymbol;
 }
+function personalCardGeneretor(width, height) {
+  const SYMBOLS = ["♦", "♥", "♠", "♣"];
+  const NUMBERS = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const randomSymbol = symbolGenerator(SYMBOLS);
+  const randomNumbers = numberGenerator(NUMBERS);
+
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+  cardDiv.style.width = width + "px";
+  cardDiv.style.height = height + "px";
+
+  const cardHeaderDiv = document.createElement("div");
+  cardHeaderDiv.className = "cardHeader text-start ms-4";
+  cardHeaderDiv.id = "personalCardHeader";
+  cardHeaderDiv.innerText = randomSymbol;
+
+  const cardBodyDiv = document.createElement("div");
+  cardBodyDiv.className = "cardBody text-center mt-5";
+  cardBodyDiv.id = "personalCardBody";
+  cardBodyDiv.innerText = randomNumbers;
+
+  const cardFooterDiv = document.createElement("div");
+  cardFooterDiv.className = "cardFooter text-end flip-vertical me-4";
+  cardFooterDiv.id = "personalCardFooter";
+  cardFooterDiv.innerText = randomSymbol;
+
+  cardDiv.appendChild(cardHeaderDiv);
+  cardDiv.appendChild(cardBodyDiv);
+  cardDiv.appendChild(cardFooterDiv);
+  cardContainer.appendChild(cardDiv);
+
+  const personalCardHeaderElement = document.getElementById(
+    "personalCardHeader"
+  );
+  const personalCardFooterElement = document.getElementById(
+    "personalCardFooter"
+  );
+  personalCardHeaderElement.classList.remove("red");
+  personalCardFooterElement.classList.remove("red");
+  if (randomSymbol === "♦" || randomSymbol === "♥") {
+    personalCardHeaderElement.classList.add("red");
+    personalCardFooterElement.classList.add("red");
+  }
+}
+function validateNumbers() {
+  const inputWidth = document.getElementById("inputWidth").value;
+  const inputHeight = document.getElementById("inputHeight").value;
+  const widthInt = Number(inputWidth);
+  const heightInt = Number(inputHeight);
+  if (widthInt >= 105 && heightInt >= 550) {
+    personalCardGeneretor(widthInt, heightInt);
+  } else {
+    alert(
+      "Por favor ingresa valores válidos. Alto minimo 550px. Ancho minimo 105px"
+    );
+  }
+}
+
 window.onload = function() {
   //write your code here
   generateCard();
@@ -51,4 +126,7 @@ window.onload = function() {
   document
     .getElementById("generateButton")
     .addEventListener("click", generateCard);
+  document
+    .getElementById("personal-generate-button")
+    .addEventListener("click", validateNumbers);
 };
